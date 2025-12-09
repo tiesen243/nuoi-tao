150dcf6b7ee54281eb5800b5c04575619728fb58
tiesen243 <ttien56906@gmail.com>
2025-12-09 23:20:11 +0700

    feat: added shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
+  typedRoutes: true,
+} satisfies NextConfig
 
-export default nextConfig;
+export default nextConfig
